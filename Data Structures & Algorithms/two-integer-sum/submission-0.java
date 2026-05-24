304436c56eeccaebe9e9ce70c854ffa3094d3cca
Jahnvi Mandle <67870408+mandlejahnvi@users.noreply.github.com>
2026-05-24 11:04:00 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len=nums.length;
+        for(int i=0;i<len;i++)
+        {
+            for(int j=i+1;j<len;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    int []res = {i,j};
+                    return res;
+                }
+            }
+        }
+        return null;
+    }
+}
