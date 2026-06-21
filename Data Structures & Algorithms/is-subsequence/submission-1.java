73e1f424dfc2b0366211293bc35b0c038e2f37fd
Jahnvi Mandle <67870408+mandlejahnvi@users.noreply.github.com>
2026-06-21 22:19:11 +0530

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0, len=s.length(); 
+        for(char ch: t.toCharArray())
+        {
+            if(i==len)
+                return true;
+            if(ch==s.charAt(i))
+            {
+                i++;
+            }
+        }
+        if(i==len)
+            return true;
+        else
+            return false;
+    }
+}
